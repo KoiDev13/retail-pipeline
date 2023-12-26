71bf73232abb16756487c16d1f66fb7304d75a6f
khoi.nguyen <knguyenanh8194@gmail.com>
2023-12-26 20:14:59 +0700

    update prequist

---
+++ dags/retail.py
@@ -0,0 +1,30 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=['retail'],
+)
+def retail():
+
+    upload_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id='upload_csv_to_gcs',
+        src='include/dataset/online_retail.csv',
+        dst='raw/online_retail.csv',
+        bucket='koi_online_retail',
+        gcp_conn_id='gcp',
+        mime_type='text/csv',
+    )
+
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id="create_retail_dataset",
+        dataset_id="retail",
+        gcp_conn_id="gcp",
+    )
+
+retail()
